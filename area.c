b7fa7045e8e8bd5178d8abe2a6c144d7d6c5d906
AryanKoundal <aryankoundal474@gmail.com>
2021-02-20 23:36:03 +0530

    this is a commit mssg as a sample on area.c

---
+++ area.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+    float r= 3;
+    float pi= 3.14;
+    printf("area of circle is %f\n",pi*r*r);
+    float h=3;
+    printf("vol of the cylinder is %f",pi*r*r*h);
+    return 0;
+}
+//either you can put formula in the statement or make its own variable ie 
+//area=pi*r*r in this case.both will work
