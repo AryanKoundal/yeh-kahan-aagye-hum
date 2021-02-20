d0da8dbaca34b5e39a130202bb6894b303f89e19
AryanKoundal <aryankoundal474@gmail.com>
2021-02-20 23:32:57 +0530

    intial commit

---
+++ first.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+/*
+yeh mera pehla c program hai, lagi padi hai
+*/
+int main(){
+    // Declaring variables to store error codes
+    int tom;
+    int TOM;
+    int error_code;
+    // '@' --> a character
+    printf("kuch ni samajh aa rha");
+    return 0;
+}
